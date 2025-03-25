826c6870d5c11059397c038f906ab9ac374c8ccc
Iván Corujo <icorujo@dtc.local>
2025-03-25 12:28:21 +0100

    Modificamos el workspaces name de main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
 	cloud { 	    
 	    organization = "terraform-test-1-icorujo" 
 	    workspaces { 
-	      name = "terraform-entrega-sprint3" 
+	      name = "terraform-entrega-sprint3-github" 
 	    } 
 	} 
 }
